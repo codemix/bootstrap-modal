8864c8abde362319697bd47e27ab6a2ffa4db631
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:35 +0100

    Added tag v2.1.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* =========================================================
-   * bootstrap-modal.js v2.1.0
+   * bootstrap-modal.js v2.1.1
    * http://twitter.github.com/bootstrap/javascript.html#modals
    * =========================================================
    * Copyright 2012 Twitter, Inc.
